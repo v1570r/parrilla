4cebdeb6abfa8ecfb2a96595d4b8a862b1dc1799
v1570r <v1570r@yahoo.es>
2024-02-28 17:41:51 +0100

    Instalación de Spring Boot.

---
+++ parriJava/src/main/java/org/parrilla/parriJava/ParriJavaApplication.java
@@ -0,0 +1,13 @@
+package org.parrilla.parriJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParriJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParriJavaApplication.class, args);
+	}
+
+}
