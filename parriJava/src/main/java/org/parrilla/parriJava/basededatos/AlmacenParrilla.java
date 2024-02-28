1bb2361c2a3238d22e37ddf286ada1eb2c99750f
v1570r <v1570r@yahoo.es>
2024-02-28 19:51:48 +0100

    Implementar /parrilla

---
+++ parriJava/src/main/java/org/parrilla/parriJava/basededatos/AlmacenParrilla.java
@@ -0,0 +1,10 @@
+package org.parrilla.parriJava.basededatos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AlmacenParrilla extends JpaRepository<TablaParrilla, Long> {
+    List<TablaParrilla> findByMomentoBetweenOrderByMomentoAsc(LocalDateTime startMomento, LocalDateTime endMomento);
+}
